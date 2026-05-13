31c4695ccf0a99d01cd81e635dbad97be1706639
AnaluFIAP <rm558793@fiap.com.br>
2026-05-12 22:19:46 -0300

    feat(): Adicionando funcionalidade as consultas

--- src/components/ConsultaCard.tsx
+++ src/components/ConsultaCard.tsx
@@ -5,7 +5,7 @@ import { Consulta } from "../interface/consulta";
 type ConsultaCardProps = {
   consulta: Consulta;
 
- onConfirmar?: () => void;
+  onConfirmar?: () => void;
 
   onCancelar?: () => void;
 };
@@ -15,8 +15,6 @@ export default function ConsultaCard({
   onConfirmar,
   onCancelar,
 }: ConsultaCardProps) {
-
-
   function formatarValor(valor: number): string {
     return valor.toLocaleString("pt-BR", {
       style: "currency",
@@ -24,8 +22,14 @@ export default function ConsultaCard({
     });
   }
 
-  function formatarData(data: Date): string {
-    return data.toLocaleDateString("pt-BR");
+  function formatarData(dataHora: string): string {
+    const data = new Date(dataHora);
+    const dia = data.toLocaleDateString("pt-BR");
+    const hora = data.toLocaleTimeString("pt-BR", {
+      hour: "2-digit",
+      minute: "2-digit",
+    });
+    return `${dia} às ${hora}`;
   }
 
   return (
@@ -37,9 +41,7 @@ export default function ConsultaCard({
           consulta.status === "cancelada" && styles.statusCancelada,
         ]}
       >
-        <Text style={styles.statusTexto}>
-          {consulta.status.toUpperCase()}
-        </Text>
+        <Text style={styles.statusTexto}>{consulta.status.toUpperCase()}</Text>
       </View>
 
       <View style={styles.secao}>
@@ -61,10 +63,8 @@ export default function ConsultaCard({
 
       <View style={styles.secao}>
         <Text style={styles.label}>📅 Dados da Consulta</Text>
-        <Text style={styles.valor}>Data: {formatarData(consulta.data)}</Text>
-        <Text style={styles.valor}>
-          Valor: {formatarValor(consulta.valor)}
-        </Text>
+        <Text>{formatarData(consulta.dataHora)}</Text>
+        <Text style={styles.valor}>Valor: {formatarValor(consulta.valor)}</Text>
         {consulta.observacoes && (
           <Text style={styles.observacoes}>{consulta.observacoes}</Text>
         )}
